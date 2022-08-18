84caf78d12fa693e58ae95be3f49e8db89a1d09a
Eirikur Jonsson <eirikurj@umich.edu>
2022-08-18 22:30:12 +0000

    Total refactor. Complexify is now a library and utils. More to come

---
+++ Makefile
@@ -0,0 +1,32 @@
+-include config.mk
+
+.PHONY: default clean install complexify_build
+
+default:
+# Check if the config.mk file is in the config dir.
+	@if [ ! -f "config/config.mk" ]; then \
+	echo "Before compiling, copy an existing config file from the "; \
+	echo "config/defaults/ directory to the config/ directory and  "; \
+	echo "rename to config.mk. For example:"; \
+	echo " ";\
+	echo "  cp config/defaults/config.LINUX_GFORTRAN.mk config/config.mk"; \
+	echo " ";\
+	echo "Then modify this config file as required. "; \
+	echo "Rerun 'make' and the build will start"; \
+	else make complexify_build;\
+	fi;
+
+clean:
+	@echo "Making clean ... "
+	rm -rf config.mk
+	rm -rf src/*.mod
+	rm -rf src/*.o
+	rm -rf src/*.so
+
+install:
+	mkdir -p ${INSTALL_DIR}/include && cp ./src/*.mod ${INSTALL_DIR}/include
+	mkdir -p ${INSTALL_DIR}/lib && cp ./src/*.so ${INSTALL_DIR}/lib
+
+complexify_build:
+	ln -sf config/config.mk config.mk;
+	(cd src && make)
---
+++ tests/test_utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+from complexify.utils import norm  #, abs, arctan2
+
+# Compute the FD reference
+a = np.array([3.0, -10.0, 2.0], dtype=np.float)
+b = np.array([1.0, 4.0, -5.0], dtype=np.float)
+
+a_copy = a.copy()
+h = 1.0e-4
+a_copy[0] += h
+
+# EX1 - Normalize vec b = a / | a |
+unit_fd = (a_copy / np.linalg.norm(a_copy) - a / np.linalg.norm(a)) / h
+
+# EX2 - obj = norm(a) * norm(b)
+obj_fd = (np.linalg.norm(a_copy) * np.linalg.norm(b) - np.linalg.norm(a) * np.linalg.norm(b)) / h
+
+# Compute CS
+h = 1.e-20
+a = np.array([3.+h*1j, -10.0, 2.0], dtype=np.complex128)
+b = np.array([1.0, 4.0, -5.0], dtype=np.complex128)
+
+# EX1
+unit_cs_wrong = a / np.linalg.norm(a)
+unit_cs = a / norm(a)
+print("FD unit:", unit_fd)
+print("CS unit wrong:", np.imag(unit_cs_wrong / h))
+print("CS unit right:", np.imag(unit_cs / h))
+
+# EX2
+obj_cs_wrong = np.linalg.norm(a) * np.linalg.norm(b)
+obj_cs = norm(a) * norm(b)
+print("FD obj:", obj_fd)
+print("CS obj wrong:", np.imag(obj_cs_wrong / h))
+print("CS obj right:", np.imag(obj_cs / h))
+
