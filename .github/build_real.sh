48e4cc9623f4d462241b8c2d23acd9806d6d6233
sseraj <sseraj@umich.edu>
2023-07-31 13:25:37 -0400

    updated install directory

--- .github/build_real.sh
+++ .github/build_real.sh
@@ -4,7 +4,7 @@ set -e
 # Export variables to make sure they are present for build.
 # The user should define them beforehand.
 if [[ -z "$COMPLEXIFY_DIR" ]]; then
-    export COMPLEXIFY_DIR=$MDOLAB_REPO_DIR/complexify/install
+    export COMPLEXIFY_DIR=$MDOLAB_REPO_DIR/complexify/opt-$COMPILERS
     export LD_LIBRARY_PATH=$COMPLEXIFY_DIR/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 fi
 
