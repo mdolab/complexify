474d1bbf733b52f4c33226a9d43158b01a6d61a6
Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
2024-05-16 17:09:49 +0000

    update intel config compiler check (#9)

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -6,7 +6,17 @@
 INSTALL_DIR = $(COMPLEXIFY_DIR)
 
 # ------- Define the compiler ------------------------------------------
-FF90 = ifort
+# Note that ";" is there to avoid make shell optimization, otherwise the shell command may fail
+ICC_EXISTS := $(shell command -v icc;)
+
+ifdef ICC_EXISTS
+  # icc only exists on older Intel versions
+  # Assume that we want to use the old compilers
+  FF90 = ifort
+else
+  # Use the new compilers
+  FF90 = ifx
+endif
 
 # ------- Define compiler flags ----------------------------------------
 FF90_FLAGS = -fPIC -O2 -r8 -stand f08
