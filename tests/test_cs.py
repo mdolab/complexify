dee944758ea74720c097112c03120c4125c48607
Alasdair Gray <alachris@umich.edu>
2026-03-02 13:48:21 -0500

    Test cs function with negative values (#14)
    * Test cs function with negative values
    
    * Formatting
    
    * Remove numpy 2 check

--- tests/test_cs.py
+++ tests/test_cs.py
@@ -9,11 +9,11 @@ class TestComplexSafe(unittest.TestCase):
     def setUp(self):
         # Generate random arrays with a fixed seed
         np.random.seed(3)
-        self.a = np.random.rand(5)
-        self.b = np.random.rand(5)
+        self.a = 2 * (np.random.rand(5) - 0.5)
+        self.b = 2 * (np.random.rand(5) - 0.5)
 
-        self.hFD = 1e-6
-        self.hCS = 1e-40
+        self.hFD = 1e-8
+        self.hCS = 1e-200
 
         self.aFD = self.a.copy()
         self.aFD[0] += self.hFD
